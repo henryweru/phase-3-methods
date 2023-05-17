f91f8006d42b9ac75f365fff7f4f9220e61ebc5a
henry <henry.waweru@student.moringaschool.com>
2023-05-17 09:29:29 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+# greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  # greet
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  # greet_with_default
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  # add
+  def add(num1, num2)
+    num1 + num2
+  end
+  
+  # halve
+  def halve(number)
+    return number / 2 if number.is_a?(Integer)
+    nil
+  end
+  
